10f9ef104cbb27a05f9f7b299fb24b8222863bfb
Liza Petkova <lizapetkova@yandex.ru>
2023-03-13 00:05:54 +0300

    fix: положение скролла компонента ConstructorList

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -5,11 +5,11 @@ import burgerConstructorStyles from './burger-constructor.module.css';
 
 export const BurgerConstructor = ({ingredients}) => {
   return (
-      <section className={cn(burgerConstructorStyles.section, 'mt-25', 'ml-4', 'mr-4', 'mb-10')}>
+      <section className={cn(burgerConstructorStyles.section, 'pt-25', 'pb-10')}>
         <ConstructorList ingredients={ingredients}/>
-        <div className={cn(burgerConstructorStyles.price, 'mt-10',)}>
+        <div className={cn(burgerConstructorStyles.price, 'mt-10', 'mr-4')}>
           <div>
-            <span className='text text_type_digits-medium mr-2'>610</span>
+            <span className='text text_type_digits-medium mr-4'>610</span>
             <CurrencyIcon type="primary"/>
           </div>
           <Button htmlType="button" type="primary" size="large">
