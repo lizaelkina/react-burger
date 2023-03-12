31054135040e32de61ce9498eeba30222a51c917
Liza Petkova <lizapetkova@yandex.ru>
2023-03-12 19:25:53 +0300

    feat: верстка компонента IngredientGroup

--- src/components/ingredient-group/ingredient-group.jsx
+++ src/components/ingredient-group/ingredient-group.jsx
@@ -1,12 +1,18 @@
+import cn from 'classnames';
 import {BurgerIngredient} from '../burger-ingredient/burger-ingredient';
+import ingredientGroupStyles from './ingredient-group.module.css';
 
 export const IngredientGroup = ({title, ingredients}) => {
   return (
       <>
-        <h2 className='text text_type_main-medium pb-6'>{title}</h2>
-        {
-          ingredients.map(ingredient => <BurgerIngredient ingredient={ingredient}/>)
-        }
+        <h2 className='text text_type_main-medium'>{title}</h2>
+        <ul className={cn(ingredientGroupStyles.group, 'pt-6', 'pl-4', 'pb-10')}>
+          {
+            ingredients.map((ingredient, index) => {
+              return <BurgerIngredient ingredient={ingredient} count={index === 0 ? 1 : 0}/>
+            })
+          }
+        </ul>
       </>
   );
 }
