88d5df21c0f0f929206e012a9699d7314aabb631
Liza Petkova <lizapetkova@yandex.ru>
2023-03-12 19:18:59 +0300

    feat: верстка компонента BurgerIngredient

--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -1,8 +1,21 @@
-export const BurgerIngredient = ({ingredient}) => {
+import cn from 'classnames';
+import {Counter, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import burgerIngredientStyles from './burger-ingredient.module.css';
+
+export const BurgerIngredient = ({ingredient, count = 0}) => {
   return (
-      <div>
-        <img src={ingredient.image} alt={ingredient.name}/>
-        <figcaption>{ingredient.name}</figcaption>
-      </div>
+      <li className={burgerIngredientStyles.card}>
+        <img className='p-4' src={ingredient.image} alt={ingredient.name}/>
+        {
+          count > 0 ? <Counter count={count} size="default" extraClass="m-1"/> : null
+        }
+        <div className={burgerIngredientStyles.card__price}>
+          <span className='text text_type_digits-default'>{ingredient.price}</span>
+          <CurrencyIcon type="primary"/>
+        </div>
+        <span className={cn(burgerIngredientStyles.card__name, 'text text_type_main-default')}>
+          {ingredient.name}
+        </span>
+      </li>
   );
 }
