e4e9db871af9bbfc3ac3a8e0b17b5594d9a13a04
Liza Petkova <lizapetkova@yandex.ru>
2023-03-12 23:12:26 +0300

    fix: выравнивание логотипа по центру хедера

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -5,26 +5,26 @@ import appHeaderStyles from './app-header.module.css';
 export const AppHeader = () => {
   return (
       <header className={cn(appHeaderStyles.header, 'pt-4', 'pb-4')}>
-        <nav className={appHeaderStyles.navigation}>
-          <ul className={appHeaderStyles.navigation__links}>
-            <li>
-              <a className={cn(appHeaderStyles.link, 'm-5')} href="#">
-                <BurgerIcon type="primary"/>
-                <span className='ml-2 text text_type_main-default text_color_primary'>Конструктор</span>
-              </a>
-            </li>
-            <li>
-              <a className={cn(appHeaderStyles.link, 'm-5')} href="#">
-                <ListIcon type="secondary"/>
-                <span className='text text_type_main-default text_color_inactive ml-2'>Лента заказов</span>
-              </a>
-            </li>
-          </ul>
-          <Logo/>
-          <a className={cn(appHeaderStyles.link, 'm-5')} href="#">
-            <ProfileIcon type="secondary"/>
-            <span className='text text_type_main-default text_color_inactive ml-2'>Личный кабинет</span>
-          </a>
+        <nav className={appHeaderStyles.nav}>
+          <div className={cn(appHeaderStyles.nav__group, appHeaderStyles.nav__group_links)}>
+            <a className={cn(appHeaderStyles.link, 'p-5', 'mr-2')} href="#">
+              <BurgerIcon type="primary"/>
+              <span className='text text_type_main-default text_color_primary ml-2'>Конструктор</span>
+            </a>
+            <a className={cn(appHeaderStyles.link, 'p-5')} href="#">
+              <ListIcon type="secondary"/>
+              <span className='text text_type_main-default text_color_inactive ml-2'>Лента заказов</span>
+            </a>
+          </div>
+          <div className={cn(appHeaderStyles.nav__group, appHeaderStyles.nav__group_logo)}>
+            <Logo/>
+          </div>
+          <div className={cn(appHeaderStyles.nav__group, appHeaderStyles.nav__group_profile)}>
+            <a className={cn(appHeaderStyles.link, 'p-5')} href="#">
+              <ProfileIcon type="secondary"/>
+              <span className='text text_type_main-default text_color_inactive ml-2'>Личный кабинет</span>
+            </a>
+          </div>
         </nav>
       </header>
   );
