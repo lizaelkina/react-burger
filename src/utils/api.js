2cf7a165a0de776a6ff3b97d5988b8cba0904cc2
Liza Petkova <lizapetkova@yandex.ru>
2023-03-13 01:26:31 +0300

    chore: добавление в html модального окна

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,15 @@
 const apiUrl = 'https://norma.nomoreparties.space/api';
 
-export const getIngredients = () => fetch(`${apiUrl}/ingredients `)
+export const getIngredients = () => fetch(`${apiUrl}/ingredients`)
     .then(response => {
       if (response.ok) {
         return response.json();
       }
-      return Promise.reject(`${response.status}: ${response.statusText}`);
+      return Promise.reject(`Ошибка получения данных: ${response.status}`);
     })
     .then(dataIngredients => {
       if (dataIngredients.success) {
         return dataIngredients.data;
       }
     })
+    .catch(error => console.log(error));
