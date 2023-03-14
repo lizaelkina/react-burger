a498c8b3ea59b1f1dcbe35ada3b80ca7965cc192
Liza Petkova <lizapetkova@yandex.ru>
2023-03-15 01:39:11 +0300

    feat: верстка и логика модального окна IngredientDetails

--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -1,21 +1,44 @@
 import cn from 'classnames';
 import {Counter, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import burgerIngredientStyles from './burger-ingredient.module.css';
+import React from "react";
+import {IngredientDetails} from "../ingredient-details/ingredient-details";
 
-export const BurgerIngredient = ({ingredient, count = 0}) => {
-  return (
-      <li className={burgerIngredientStyles.card}>
-        <img className='p-4' src={ingredient.image} alt={ingredient.name}/>
-        {
-          count > 0 ? <Counter count={count} size="default" extraClass="m-1"/> : null
-        }
-        <div className={burgerIngredientStyles.card__price}>
-          <span className='text text_type_digits-default'>{ingredient.price}</span>
-          <CurrencyIcon type="primary"/>
-        </div>
-        <span className={cn(burgerIngredientStyles.card__name, 'text text_type_main-default')}>
+export class BurgerIngredient extends React.Component {
+  state = {
+    isOpenModal: false,
+  };
+
+  handleOpenModal = () => {
+    this.setState({isOpenModal: true});
+  }
+
+  handleCloseModal = () => {
+    this.setState({isOpenModal: false});
+  }
+
+
+  render() {
+    let {ingredient, count} = this.props;
+
+    return (
+        <>
+          <li className={burgerIngredientStyles.card} onClick={this.handleOpenModal}>
+            <img className='p-4' src={ingredient.image} alt={ingredient.name}/>
+            {
+              count > 0 ? <Counter count={count} size="default" extraClass="m-1"/> : null
+            }
+            <div className={burgerIngredientStyles.card__price}>
+              <span className='text text_type_digits-default'>{ingredient.price}</span>
+              <CurrencyIcon type="primary"/>
+            </div>
+            <span className={cn(burgerIngredientStyles.card__name, 'text text_type_main-default')}>
           {ingredient.name}
-        </span>
-      </li>
-  );
+          </span>
+          </li>
+          {this.state.isOpenModal && <IngredientDetails ingredient={ingredient} onClose={this.handleCloseModal}/>}
+        </>
+
+    );
+  }
 }
--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,5 +1,34 @@
-export const IngredientDetails = () => {
+import React from 'react';
+import cn from 'classnames';
+import {Modal} from '../modal/modal';
+import ingredientDetailsStyles from './ingredient-details.module.css';
+
+export const IngredientDetails = ({ingredient, onClose}) => {
+
   return (
-      <h2>IngredientDetails</h2>
+      <Modal title={'Детали ингредиента'} onClose={onClose}>
+        <div className={ingredientDetailsStyles.card}>
+          <img className={ingredientDetailsStyles.card__image} src={ingredient.image_large} alt={'Ингредиент'}/>
+          <span className="text text_type_main-medium mt-4 mb-8">{ingredient.name}</span>
+          <div className={ingredientDetailsStyles.card__info}>
+            <div className={cn(ingredientDetailsStyles.card__compound)}>
+              <span className="text text_type_main-default text_color_inactive">Калории, ккал</span>
+              <span className="text text_type_digits-default text_color_inactive">{ingredient.calories}</span>
+            </div>
+            <div className={cn(ingredientDetailsStyles.card__compound)}>
+              <span className="text text_type_main-default text_color_inactive">Белки, г</span>
+              <span className="text text_type_digits-default text_color_inactive">{ingredient.proteins}</span>
+            </div>
+            <div className={cn(ingredientDetailsStyles.card__compound)}>
+              <span className="text text_type_main-default text_color_inactive">Жиры, г</span>
+              <span className="text text_type_digits-default text_color_inactive">{ingredient.fat}</span>
+            </div>
+            <div className={ingredientDetailsStyles.card__compound}>
+              <span className="text text_type_main-default text_color_inactive">Углеводы, г</span>
+              <span className="text text_type_digits-default text_color_inactive">{ingredient.carbohydrates}</span>
+            </div>
+          </div>
+        </div>
+      </Modal>
   );
 }
