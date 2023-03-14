ff252c31d5dd1333de419e2261bfaa5313191a57
Liza Petkova <lizapetkova@yandex.ru>
2023-03-15 02:56:37 +0300

    fix: верстка модального окна заказа

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,21 +1,40 @@
+import React from 'react';
 import cn from 'classnames';
 import {Button, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import {ConstructorList} from '../constructor-list/constructor-list';
+import {OrderDetails} from '../order-details/order-details';
 import burgerConstructorStyles from './burger-constructor.module.css';
 
-export const BurgerConstructor = ({ingredients}) => {
-  return (
-      <section className={cn(burgerConstructorStyles.section, 'pt-25', 'pl-3', 'pb-10')}>
-        <ConstructorList ingredients={ingredients}/>
-        <div className={cn(burgerConstructorStyles.price, 'mt-10', 'mr-4')}>
-          <div>
-            <span className='text text_type_digits-medium mr-4'>610</span>
-            <CurrencyIcon type="primary"/>
+export class BurgerConstructor extends React.Component {
+  state = {
+    isOpenModal: false,
+  };
+
+  handleOpenModal = () => {
+    this.setState({isOpenModal: true});
+  }
+
+  handleCloseModal = () => {
+    this.setState({isOpenModal: false});
+  }
+
+  render() {
+    let {ingredients} = this.props;
+
+    return (
+        <section className={cn(burgerConstructorStyles.section, 'pt-25', 'pl-3', 'pb-10')}>
+          <ConstructorList ingredients={ingredients}/>
+          <div className={cn(burgerConstructorStyles.price, 'mt-10', 'mr-4')}>
+            <div>
+              <span className='text text_type_digits-medium mr-4'>610</span>
+              <CurrencyIcon type="primary"/>
+            </div>
+            <Button htmlType="button" type="primary" size="large" onClick={this.handleOpenModal}>
+              Оформить заказ
+            </Button>
+            {this.state.isOpenModal && <OrderDetails onClose={this.handleCloseModal}/>}
           </div>
-          <Button htmlType="button" type="primary" size="large">
-            Оформить заказ
-          </Button>
-        </div>
-      </section>
-  );
+        </section>
+    );
+  }
 }
--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,5 +1,20 @@
-export const OrderDetails = () => {
+import cn from 'classnames';
+import {CheckMarkIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import {Modal} from '../modal/modal';
+import orderDetailsStyles from './order-details.module.css';
+
+export const OrderDetails = ({onClose}) => {
   return (
-      <h2>OrderDetails</h2>
+      <Modal title={''} onClose={onClose}>
+        <div className={cn(orderDetailsStyles.card, 'pt-30', 'pl-25', 'pr-25', 'pb-30')}>
+          <span className='text text_type_digits-large mb-8'>034536</span>
+          <span className='text text_type_main-medium mb-15'>идентификатор заказа</span>
+          <CheckMarkIcon type="primary"/>
+          <span className='text text_type_main-default mt-15 mb-2'>Ваш заказ начали готовить</span>
+          <span
+              className='text text_type_main-default text_color_inactive'>Дождитесь готовности на орбитальной станции
+          </span>
+        </div>
+      </Modal>
   );
 }
