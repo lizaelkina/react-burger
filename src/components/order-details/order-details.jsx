76db021163bd430e549ca66ebbc53a48c11d5267
Liza Petkova <lizapetkova@yandex.ru>
2023-03-15 23:10:19 +0300

    style: добавление анимации в окне заказа

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -8,14 +8,15 @@ export const OrderDetails = ({onClose}) => {
   return (
       <Modal onClose={onClose}>
         <div className={cn(orderDetailsStyles.card, 'pt-30 pl-25 pr-25 pb-30')}>
-          <span className='text text_type_digits-large mb-8'>034536</span>
-          <span className='text text_type_main-medium mb-15'>идентификатор заказа</span>
-          <div className={orderDetailsStyles.icon}>
+          <span className={cn(orderDetailsStyles.shadow, 'text text_type_digits-large mb-8')}>034536</span>
+          <span className='text text_type_main-medium'>идентификатор заказа</span>
+          <div className={cn(orderDetailsStyles.done, 'mt-15 mb-15')}>
             <CheckMarkIcon type='primary'/>
-            <div className={cn(orderDetailsStyles.icon, orderDetailsStyles.icon1)}></div>
-            <div className={cn(orderDetailsStyles.icon, orderDetailsStyles.icon2)}></div>
+            <div className={cn(orderDetailsStyles.figure, orderDetailsStyles.pentagon)}></div>
+            <div className={cn(orderDetailsStyles.figure, orderDetailsStyles.hexagon)}></div>
+            <div className={cn(orderDetailsStyles.figure, orderDetailsStyles.polygon)}></div>
           </div>
-          <span className='text text_type_main-default mt-15 mb-2'>Ваш заказ начали готовить</span>
+          <span className='text text_type_main-default mb-2'>Ваш заказ начали готовить</span>
           <span
               className='text text_type_main-default text_color_inactive'>Дождитесь готовности на орбитальной станции
           </span>
