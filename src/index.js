44bd8583fa7e09dc64b20b8767b7212dffdb2022
Liza Petkova <lizapetkova@yandex.ru>
2023-03-20 18:05:30 +0300

    fix: ошибка 18го реакта

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import {App} from './components/app/app';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
     <React.StrictMode>
       <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
