dcaff686fc7cdb854719e19a74193c7c147c7ced
Liza Petkova <lizapetkova@yandex.ru>
2023-03-20 21:39:51 +0300

    fix: удаление компонента Modal внутри компонента с деталями заказа

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -3,35 +3,37 @@ import cn from 'classnames';
 import {Button, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import {ConstructorList} from '../constructor-list/constructor-list';
 import {OrderDetails} from '../order-details/order-details';
+import {Modal} from '../modal/modal';
 import {ingredientsArrayTypes} from '../../utils/prop-types';
 import burgerConstructorStyles from './burger-constructor.module.css';
 
 export const BurgerConstructor = ({ingredients}) => {
 
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
-  function handleOpenModal() {
-    setIsOpenModal(true);
-  }
-
-  function handleCloseModal() {
-    setIsOpenModal(false);
-  }
+  const [isOpenOrderModal, setIsOpenOrderModal] = useState(false);
 
   return (
-      <section className={cn(burgerConstructorStyles.section, 'pt-25 pl-3 pb-10')} aria-label='Конструктор бургера'>
-        <ConstructorList ingredients={ingredients}/>
-        <div className={cn(burgerConstructorStyles.price, 'mt-10 mr-4')}>
-          <div>
-            <span className='text text_type_digits-medium mr-4'>610</span>
-            <CurrencyIcon type='primary'/>
+      <>
+        <section className={cn(burgerConstructorStyles.section, 'pt-25 pl-3 pb-10')} aria-label='Конструктор бургера'>
+          <ConstructorList ingredients={ingredients}/>
+          <div className={cn(burgerConstructorStyles.price, 'mt-10 mr-4')}>
+            <div>
+              <span className='text text_type_digits-medium mr-4'>610</span>
+              <CurrencyIcon type='primary'/>
+            </div>
+            <Button htmlType='button'
+                    type='primary'
+                    size='large'
+                    onClick={() => setIsOpenOrderModal(true)}>
+              Оформить заказ
+            </Button>
           </div>
-          <Button htmlType='button' type='primary' size='large' onClick={handleOpenModal}>
-            Оформить заказ
-          </Button>
-          {isOpenModal && <OrderDetails onClose={handleCloseModal}/>}
-        </div>
-      </section>
+        </section>
+
+        {isOpenOrderModal &&
+            <Modal onClose={() => setIsOpenOrderModal(false)}>
+              <OrderDetails/>
+            </Modal>}
+      </>
   );
 }
 
