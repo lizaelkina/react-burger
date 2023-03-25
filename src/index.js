a4fc56b12ddf6acccaeadba78387374b7bbd1e83
Liza Petkova <lizapetkova@yandex.ru>
2023-03-25 20:57:49 +0300

    feat: подключение расширения redux devtools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
-import {createStore} from 'redux';
+import {compose, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {rootReducer} from './services/reducers';
 import {App} from './components/app/app';
 import './index.css';
 
-const store = createStore(rootReducer);
+const composeEnhancers =
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+        : compose;
+
+const enhancer = composeEnhancers();
+const store = createStore(rootReducer, enhancer);
 
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
