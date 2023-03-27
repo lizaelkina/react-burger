7410f349f290ded0435c9127a9322c5a0da07863
Liza Petkova <lizapetkova@yandex.ru>
2023-03-27 21:21:21 +0300

    feat: добавление навигации по ингредиентам

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useMemo} from 'react';
+import {useEffect, useMemo, useRef} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import cn from 'classnames';
 import {Tab} from '@ya.praktikum/react-developer-burger-ui-components';
@@ -14,6 +14,7 @@ import burgerIngredientsStyles from './burger-ingredients.module.css';
 export const BurgerIngredients = () => {
 
   const dispatch = useDispatch();
+  const scrollRef = useRef(null);
 
   useEffect(() => {
     dispatch(loadIngredients())
@@ -35,6 +36,29 @@ export const BurgerIngredients = () => {
     dispatch(selectIngredientGroup(value));
     const title = document.getElementById(value);
     if (title) title.scrollIntoView({behavior: 'smooth'});
+  }
+
+  function handleScroll() {
+    const data = [
+      {
+        value: 'bun',
+        offset: Math.abs(document.getElementById('bun').offsetTop - scrollRef.current.scrollTop)
+      },
+      {
+        value: 'sauce',
+        offset: Math.abs(document.getElementById('sauce').offsetTop - scrollRef.current.scrollTop)
+      },
+      {
+        value: 'main',
+        offset: Math.abs(document.getElementById('main').offsetTop - scrollRef.current.scrollTop)
+      },
+    ];
+
+    const closestElement = data.reduce(
+        (prev, current) =>
+            prev.offset < current.offset ? prev : current,
+        data[0]);
+    dispatch(selectIngredientGroup(closestElement.value));
   }
 
   return (
@@ -61,7 +85,9 @@ export const BurgerIngredients = () => {
                     Начинки
                   </Tab>
                 </div>
-                <div className={cn(burgerIngredientsStyles.scroll, 'custom-scroll')}>
+                <div className={cn(burgerIngredientsStyles.scroll, 'custom-scroll')}
+                     ref={scrollRef}
+                     onScroll={handleScroll}>
                   <IngredientGroup title='Булки' ingredients={bun} id='bun'/>
                   <IngredientGroup title='Соусы' ingredients={sauce} id='sauce'/>
                   <IngredientGroup title='Начинки' ingredients={main} id='main'/>
