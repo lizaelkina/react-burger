9a5abd47ffcbcca959da7b0c0ca5f4a3d1989b52
Liza Petkova <lizapetkova@yandex.ru>
2023-03-30 02:57:16 +0300

    feat: верстка экрана восстановления пароля

---
+++ src/components/login-form/reset-password-form/reset-password-form.jsx
@@ -0,0 +1,62 @@
+import {useRef, useState} from 'react';
+import cn from 'classnames';
+import {Button, Input} from '@ya.praktikum/react-developer-burger-ui-components';
+import resetFormStyles from './reset-password-form.module.css';
+
+export const AuthorizationForm = () => {
+
+  const [valueInput, setValueInput] = useState('');
+  const inputRef = useRef(null);
+  const onIconClick = () => {
+    setTimeout(() => inputRef.current.focus(), 0)
+    alert('Icon Click Callback')
+  }
+
+  return (
+      <section className={cn(resetFormStyles.card)}>
+        <span className='text text_type_main-medium mb-6'>Восстановление пароля</span>
+        <form className={cn(resetFormStyles.form, 'mb-20')}>
+          <Input autocomplete="off"
+                 name={'password'}
+                 value={valueInput}
+                 type={'text'}
+                 placeholder={'Введите новый пароль'}
+                 error={false}
+                 errorText={'Слишком простой пароль'}
+                 ref={inputRef}
+                 size={'default'}
+                 icon={'ShowIcon'}
+                 onChange={e => setValueInput(e.target.value)}
+                 onIconClick={onIconClick}
+          />
+          <Input autocomplete="off"
+                 name={'password'}
+                 value={valueInput}
+                 type={'text'}
+                 placeholder={'Введите код из письма'}
+                 error={false}
+                 errorText={'Неверный код'}
+                 ref={inputRef}
+                 size={'default'}
+                 onChange={e => setValueInput(e.target.value)}
+                 onIconClick={onIconClick}
+          />
+          <Button extraClass={cn(resetFormStyles.button_primary)}
+                  htmlType='submit'
+                  type='primary'
+                  size='medium'>
+            Сохранить
+          </Button>
+        </form>
+        <div>
+          <span className='text text_type_main-default text_color_inactive'>Вспомнили пароль?</span>
+          <Button extraClass={cn(resetFormStyles.button_secondary, 'ml-2')}
+                  htmlType='button'
+                  type='secondary'
+                  size='medium'>
+            Войти
+          </Button>
+        </div>
+      </section>
+  )
+}
