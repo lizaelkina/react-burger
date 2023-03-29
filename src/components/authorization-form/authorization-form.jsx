f5bfc2c2e0ad42f016eda02fc63d125955901395
Liza Petkova <lizapetkova@yandex.ru>
2023-03-30 02:05:54 +0300

    feat: верстка экрана авторизации

---
+++ src/components/authorization-form/authorization-form.jsx
@@ -0,0 +1,69 @@
+import {useRef, useState} from 'react';
+import cn from 'classnames';
+import {Button, EmailInput, Input} from '@ya.praktikum/react-developer-burger-ui-components';
+import authorizationFormStyles from './authorization-form.module.css';
+
+export const AuthorizationForm = () => {
+
+  const [valueEmail, setValueEmail] = useState('');
+  const onChange = e => {
+    setValueEmail(e.target.value)
+  }
+
+  const [valueInput, setValueInput] = useState('');
+  const inputRef = useRef(null);
+  const onIconClick = () => {
+    setTimeout(() => inputRef.current.focus(), 0)
+    alert('Icon Click Callback')
+  }
+
+  return (
+      <section className={cn(authorizationFormStyles.card)}>
+        <span className='text text_type_main-medium mb-6'>Вход</span>
+        <form className={cn(authorizationFormStyles.form, 'mb-20')}>
+          <EmailInput name={'email'}
+                      value={valueEmail}
+                      size={'default'}
+                      isIcon={false}
+                      onChange={e => onChange}
+          />
+          <Input name={'password'}
+                 value={valueInput}
+                 type={'text'}
+                 placeholder={'Пароль'}
+                 error={false}
+                 errorText={'Ошибка'}
+                 ref={inputRef}
+                 size={'default'}
+                 icon={'ShowIcon'}
+                 onChange={e => setValueInput(e.target.value)}
+                 onIconClick={onIconClick}
+          />
+          <Button extraClass={cn(authorizationFormStyles.button_primary)}
+                  htmlType='button'
+                  type='primary'
+                  size='medium'>
+            Войти
+          </Button>
+        </form>
+        <div>
+          <span className='text text_type_main-default text_color_inactive'>Вы — новый пользователь?</span>
+          <Button extraClass={cn(authorizationFormStyles.button_secondary, 'ml-2')}
+                  htmlType='button'
+                  type='secondary'
+                  size='medium'>
+            Зарегистрироваться
+          </Button>
+        </div>
+        <div>
+          <span className='text text_type_main-default text_color_inactive'>Забыли пароль?</span>
+          <Button extraClass={cn(authorizationFormStyles.button_secondary, 'ml-2')}
+                  htmlType='button'
+                  type='secondary'
+                  size='medium'>
+            Восстановить пароль
+          </Button>
+        </div>
+      </section>
+  )
+}
