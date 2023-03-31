b313e45a7d62b95d92948ddc4d66b7f7b4f684eb
Liza Petkova <lizapetkova@yandex.ru>
2023-03-31 14:48:34 +0300

    refactor: создание отдельного компонента в окне заказа

--- src/components/burger-constructor/order-details/order-details.jsx
+++ src/components/burger-constructor/order-details/order-details.jsx
@@ -1,6 +1,6 @@
 import {useSelector} from 'react-redux';
 import cn from 'classnames';
-import {CheckMarkIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import {SuccessStatus} from './success-status/success-status';
 import {Loader} from '../../shared/loader/loader';
 import {ErrorMessage} from '../../shared/error-message/error-message';
 import orderDetailsStyles from './order-details.module.css';
@@ -12,10 +12,10 @@ export const OrderDetails = () => {
     success: store.createOrder.success,
     number: store.createOrder.number,
     errorMessage: store.createOrder.errorMessage,
-  }))
+  }));
 
   return (
-      <section className={cn(orderDetailsStyles.card, 'pt-30 pl-25 pr-25 pb-30')}>
+      <section className={cn(orderDetailsStyles.container, 'pt-30 pl-25 pr-25 pb-30')}>
         {isLoading &&
             <div className={orderDetailsStyles.load}>
               <p className='text text_type_main-medium'>Формирование заказа...</p>
@@ -29,14 +29,10 @@ export const OrderDetails = () => {
                 {number}
               </span>
               <h3 className='text text_type_main-medium'>идентификатор заказа</h3>
-              <div className={cn(orderDetailsStyles.done, 'mt-15 mb-15')}>
-                <CheckMarkIcon type='primary'/>
-                <div className={cn(orderDetailsStyles.figure, orderDetailsStyles.pentagon)}></div>
-                <div className={cn(orderDetailsStyles.figure, orderDetailsStyles.hexagon)}></div>
-                <div className={cn(orderDetailsStyles.figure, orderDetailsStyles.polygon)}></div>
-              </div>
+              <SuccessStatus/>
               <p className='text text_type_main-default mb-2'>Ваш заказ начали готовить</p>
-              <p className='text text_type_main-default text_color_inactive'>Дождитесь готовности на орбитальной станции</p>
+              <p className='text text_type_main-default text_color_inactive'>Дождитесь готовности на орбитальной
+                станции</p>
             </>
         }
       </section>
