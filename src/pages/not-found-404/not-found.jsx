d5c9fef7b183151f4eda6bfd3cd60c59980363fe
Liza Petkova <lizapetkova@yandex.ru>
2023-04-04 22:13:03 +0300

    feat: навигация на 404 странице

--- src/pages/not-found-404/not-found.jsx
+++ src/pages/not-found-404/not-found.jsx
@@ -1,8 +1,20 @@
 import cn from 'classnames';
+import {useNavigate} from 'react-router-dom';
 import {Button} from '@ya.praktikum/react-developer-burger-ui-components';
 import notFoundStyles from './not-found.module.css';
 
 export const NotFound404 = () => {
+
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate('/');
+  }
+
+  const goBack = () => {
+    navigate(-1);
+  }
+
   return (
       <section className={notFoundStyles.page}>
         <h1 className='text text_type_digits-large text_color_inactive'>404</h1>
@@ -12,11 +24,20 @@ export const NotFound404 = () => {
             К сожалению, данная страница не существует или временно недоступна.
           </span>
           <span className='text text_type_main-default text_color_active'>
-            Вы можете повторить попытку позже или перейти на
+            Вы можете повторить попытку позже, перейти
+            <Button extraClass={cn(notFoundStyles.button, 'ml-2 mr-2')}
+                    htmlType='button'
+                    type='secondary'
+                    size='medium'
+                    onClick={goBack}>
+              назад
+            </Button>
+            или перейти на
             <Button extraClass={cn(notFoundStyles.button, 'ml-2')}
                     htmlType='button'
                     type='secondary'
-                    size='medium'>
+                    size='medium'
+                    onClick={goHome}>
               главную страницу
             </Button>.
           </span>
