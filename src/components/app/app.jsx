554d2c384f2cf1c69f79fb0acdd2130734c0c5b2
Liza Petkova <lizapetkova@yandex.ru>
2023-04-04 20:55:57 +0300

    feat: настройка роутинга страниц

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,13 @@
+import {Route, Routes} from 'react-router';
 import {AppHeader} from '../shared/app-header/app-header';
 import {HomePage} from '../../pages/home/home';
-// import {IngredientPage} from '../../pages/ingredient-info/ingredient-info';
-// import {ProfilePage} from '../../pages/profile/profile';
-// import {LoginPage} from '../../pages/login/login';
-// import {RegisterPage} from '../../pages/register/register';
-// import {ForgotPasswordPage} from '../../pages/forgot-password/forgot-password';
-// import {ResetPasswordPage} from '../../pages/reset-password/reset-password';
-// import {NotFound404} from '../../pages/not-found-404/not-found';
+import {IngredientPage} from '../../pages/ingredient-info/ingredient-info';
+import {ProfilePage} from '../../pages/profile/profile';
+import {LoginPage} from '../../pages/login/login';
+import {RegisterPage} from '../../pages/register/register';
+import {ForgotPasswordPage} from '../../pages/forgot-password/forgot-password';
+import {ResetPasswordPage} from '../../pages/reset-password/reset-password';
+import {NotFound404} from '../../pages/not-found-404/not-found';
 import appStyles from './app.module.css';
 
 export const App = () => {
@@ -15,14 +16,16 @@ export const App = () => {
       <div className={appStyles.page}>
         <AppHeader/>
         <main className={appStyles.main}>
-          <HomePage/>
-          {/*<IngredientPage/>*/}
-          {/*<ProfilePage/>*/}
-          {/*<LoginPage/>*/}
-          {/*<RegisterPage/>*/}
-          {/*<ForgotPasswordPage/>*/}
-          {/*<ResetPasswordPage/>*/}
-          {/*<NotFound404/>*/}
+          <Routes>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/ingredients/:id' element={<IngredientPage/>}/>
+            <Route path='/profile' element={<ProfilePage/>}/>
+            <Route path='/login' element={<LoginPage/>}/>
+            <Route path='/register' element={<RegisterPage/>}/>
+            <Route path='/forgot-password' element={<ForgotPasswordPage/>}/>
+            <Route path='/reset-password' element={<ResetPasswordPage/>}/>
+            <Route path='*' element={<NotFound404/>}/>
+          </Routes>
         </main>
       </div>
   );
