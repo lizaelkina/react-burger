7c837bdee12b23991608c1a779ba3de504fb831f
Liza Petkova <lizapetkova@yandex.ru>
2023-04-05 17:49:07 +0300

    feat: отображение отдельной страницы ингредиента

--- src/pages/ingredient-info/ingredient-info.jsx
+++ src/pages/ingredient-info/ingredient-info.jsx
@@ -1,17 +1,30 @@
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useParams} from 'react-router';
+import {loadIngredients} from '../../services/actions/burger-ingredients';
 import {IngredientDetails} from '../../components/shared/ingredient-details/ingredient-details';
-import {ingredientPropTypes} from '../../utils/prop-types';
 import ingredientPageStyles from './ingredient-info.module.css';
 
-export const IngredientPage = ({ingredient}) => {
+export const IngredientPage = () => {
+
+  const dispatch = useDispatch();
+
+  const {ingredients} = useSelector(store => ({
+    ingredients: store.burgerIngredients.ingredients,
+  }))
+
+  useEffect(() => {
+    dispatch(loadIngredients())
+  }, [dispatch]);
+
+  const {id} = useParams();
+
+  const ingredient = ingredients.find(ingredient => ingredient._id === id);
 
   return (
       <section className={ingredientPageStyles.page}>
         <h2 className='text text_type_main-large'>Детали ингредиента</h2>
-        <IngredientDetails ingredient={ingredient}/>
+        {ingredient && <IngredientDetails ingredient={ingredient}/>}
       </section>
   )
-}
-
-IngredientPage.propTypes = {
-  ingredient: ingredientPropTypes.isRequired,
 }
