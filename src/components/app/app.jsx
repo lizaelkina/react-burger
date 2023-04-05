3dd68eb791548bb26c231e8114d41f4e93cddcb5
Liza Petkova <lizapetkova@yandex.ru>
2023-04-05 17:47:01 +0300

    feat: изменение структуры страниц

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,6 +3,7 @@ import {AppHeader} from '../shared/app-header/app-header';
 import {HomePage} from '../../pages/home/home';
 import {IngredientPage} from '../../pages/ingredient-info/ingredient-info';
 import {ProfilePage} from '../../pages/profile/profile';
+import {OrdersPage} from '../../pages/profile/orders/orders';
 import {LoginPage} from '../../pages/login/login';
 import {RegisterPage} from '../../pages/register/register';
 import {ForgotPasswordPage} from '../../pages/forgot-password/forgot-password';
@@ -20,6 +21,7 @@ export const App = () => {
             <Route path='/' element={<HomePage/>}/>
             <Route path='/ingredients/:id' element={<IngredientPage/>}/>
             <Route path='/profile' element={<ProfilePage/>}/>
+            <Route path='/profile/orders' element={<OrdersPage/>}/>
             <Route path='/login' element={<LoginPage/>}/>
             <Route path='/register' element={<RegisterPage/>}/>
             <Route path='/forgot-password' element={<ForgotPasswordPage/>}/>
