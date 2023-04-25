b8a78facfce87935cc7c9a8d8cd01b86d7382305
Liza Petkova <lizapetkova@yandex.ru>
2023-04-25 18:20:58 +0300

    fix: loader на странице статистики заказов

--- src/components/order-feed/order-sum/order-sum.tsx
+++ src/components/order-feed/order-sum/order-sum.tsx
@@ -1,7 +1,6 @@
 import {useMemo} from 'react';
 import cn from 'classnames';
 import {useAppSelector} from '../../../services/hooks';
-import {Loader} from '../../shared/loader/loader';
 import orderSumStyles from './order-sum.module.css';
 
 export const OrderSum = () => {
@@ -18,48 +17,45 @@ export const OrderSum = () => {
   const pendingOrders = useMemo(() => orders?.filter(order => order.status === 'pending').slice(0, 30), [orders]);
 
   return (
-      <>
-        {isLoading && <Loader/>}
-        <article className={cn(orderSumStyles.section)}>
-          <div className={orderSumStyles.container}>
-            <div className={orderSumStyles.column}>
-              <h5 className='text text_type_main-medium text_color_primary mb-6'>Готовы:</h5>
-              <ul className={orderSumStyles.list}>
-                {doneOrders?.map(order => {
-                  return (<li className='text text_type_digits-default text_color_success'
-                              key={order.number}>{order.number}</li>)
-                })}
-              </ul>
-            </div>
-            <div className={orderSumStyles.column}>
-              <h5 className='text text_type_main-medium text_color_primary mb-6'>В работе:</h5>
-              <ul className={orderSumStyles.list}>
-                {pendingOrders?.map(order => {
-                  return (<li className='text text_type_digits-default'
-                              key={order.number}>{order.number}</li>)
-                })}
-              </ul>
-            </div>
+      <article className={cn(orderSumStyles.section)}>
+        <div className={orderSumStyles.container}>
+          <div className={orderSumStyles.column}>
+            <h5 className='text text_type_main-medium text_color_primary mb-6'>Готовы:</h5>
+            <ul className={orderSumStyles.list}>
+              {doneOrders?.map(order => {
+                return (<li className='text text_type_digits-default text_color_success'
+                            key={order.number}>{order.number}</li>)
+              })}
+            </ul>
           </div>
-          <div className={orderSumStyles.string}>
-            <h5 className='text text_type_main-medium text_color_primary'>Выполнено за все время:</h5>
-            <span className={cn(orderSumStyles.shadow, 'text text_type_digits-large')}>{total}</span>
-            {!isLoading && !success &&
-                <span className={cn(orderSumStyles.empty, 'text text_type_main-default text_color_inactive')}>
+          <div className={orderSumStyles.column}>
+            <h5 className='text text_type_main-medium text_color_primary mb-6'>В работе:</h5>
+            <ul className={orderSumStyles.list}>
+              {pendingOrders?.map(order => {
+                return (<li className='text text_type_digits-default'
+                            key={order.number}>{order.number}</li>)
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className={orderSumStyles.string}>
+          <h5 className='text text_type_main-medium text_color_primary'>Выполнено за все время:</h5>
+          <span className={cn(orderSumStyles.shadow, 'text text_type_digits-large')}>{total}</span>
+          {!isLoading && !success &&
+              <span className={cn(orderSumStyles.empty, 'text text_type_main-default text_color_inactive')}>
                   Нет данных
                 </span>
-            }
-          </div>
-          <div className={orderSumStyles.string}>
-            <h5 className='text text_type_main-medium text_color_primary'>Выполнено за сегодня:</h5>
-            <span className={cn(orderSumStyles.shadow, 'text text_type_digits-large')}>{totalToday}</span>
-            {!isLoading && !success &&
-                <span className={cn(orderSumStyles.empty, 'text text_type_main-default text_color_inactive')}>
+          }
+        </div>
+        <div className={orderSumStyles.string}>
+          <h5 className='text text_type_main-medium text_color_primary'>Выполнено за сегодня:</h5>
+          <span className={cn(orderSumStyles.shadow, 'text text_type_digits-large')}>{totalToday}</span>
+          {!isLoading && !success &&
+              <span className={cn(orderSumStyles.empty, 'text text_type_main-default text_color_inactive')}>
                   Нет данных
                 </span>
-            }
-          </div>
-        </article>
-      </>
+          }
+        </div>
+      </article>
   );
 }
