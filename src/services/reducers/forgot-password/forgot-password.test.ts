aae9e9f569ce0658953419b8c629d98bb77344aa
Liza Petkova <lizapetkova@yandex.ru>
2023-04-29 18:55:58 +0300

    feat: покрытие тестами createForgotPasswordReducer

--- src/services/reducers/forgot-password/forgot-password.test.ts
+++ src/services/reducers/forgot-password/forgot-password.test.ts
@@ -1,7 +1,79 @@
 import {createForgotPasswordReducer, initialState} from './forgot-password';
+import {
+  changeEmail,
+  FORGOTPASSWORD_FAILED,
+  FORGOTPASSWORD_LOADING,
+  FORGOTPASSWORD_SUCCESS
+} from '../../actions/forgot-password';
 
-describe('Check forgot-password reducer', () => {
-  test('Should return initial state', () => {
+describe('check forgot-password reducer', () => {
+  it('should return initial state', () => {
     expect(createForgotPasswordReducer(undefined, {} as any)).toEqual(initialState);
   });
+
+  it('should return loading state', () => {
+    expect(
+        createForgotPasswordReducer(
+            initialState,
+            {
+              type: FORGOTPASSWORD_LOADING,
+            }
+        )
+    ).toEqual({
+      ...initialState,
+      isLoading: true,
+      success: false,
+      errorMessage: null,
+    });
+  });
+
+  it('should return success state', () => {
+    expect(
+        createForgotPasswordReducer(
+            initialState,
+            {
+              type: FORGOTPASSWORD_SUCCESS,
+            }
+        )
+    ).toEqual({
+      ...initialState,
+      isLoading: false,
+      success: true,
+    });
+  });
+
+  it('should return failed state', () => {
+    const testError = 'testError';
+    expect(
+        createForgotPasswordReducer(
+            initialState,
+            {
+              type: FORGOTPASSWORD_FAILED,
+              error: testError,
+            }
+        )
+    ).toEqual({
+      ...initialState,
+      isLoading: false,
+      errorMessage: testError,
+    });
+  });
+
+  it('should return change email state', () => {
+    expect(
+        createForgotPasswordReducer(
+            initialState,
+            changeEmail('test@mail.ru', true)
+        )
+    ).toEqual({
+      ...initialState,
+      formData: {
+        email: 'test@mail.ru',
+      },
+      formValidity: {
+        email: true,
+      },
+    });
+  });
+
 });
