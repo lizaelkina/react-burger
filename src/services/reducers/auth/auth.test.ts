4c8a5c11cbdf9dfc3d33908cd89b8e595a1b813a
Liza Petkova <lizapetkova@yandex.ru>
2023-04-29 17:18:06 +0300

    feat: покрытие тестами createAuthReducer

--- src/services/reducers/auth/auth.test.ts
+++ src/services/reducers/auth/auth.test.ts
@@ -1,7 +1,94 @@
 import {createAuthReducer, initialState} from './auth';
+import {
+  AUTH_USER,
+  CHECK_USER_FAILED,
+  CHECK_USER_SUCCESS,
+  LOGOUT_FAILED,
+  LOGOUT_LOADING,
+  LOGOUT_SUCCESS
+} from '../../actions/auth';
+import {IUser} from '../../../utils/data-types';
 
-describe('Check auth reducer', () => {
-  test('Should return initial state', () => {
+const testUser: IUser = {
+  email: 'test@mail.ru',
+  name: 'Тест',
+  password: '123456',
+};
+
+const testError: string = 'testError';
+
+describe('check auth reducer', () => {
+  it('should return initial state', () => {
     expect(createAuthReducer(undefined, {} as any)).toEqual(initialState);
   });
+
+  it('should return auth user state', () => {
+    const accessTokenTest = 'accessTokenTest';
+    const refreshTokenTest = 'refreshTokenTest';
+    expect(createAuthReducer(initialState, {
+      type: AUTH_USER,
+      user: testUser,
+      accessToken: accessTokenTest,
+      refreshToken: refreshTokenTest,
+    })).toEqual({
+      ...initialState,
+      user: testUser,
+      isAuthChecked: true,
+    });
+  });
+
+  it('should return check user success state', () => {
+    expect(createAuthReducer(initialState, {
+      type: CHECK_USER_SUCCESS,
+      user: testUser,
+    })).toEqual({
+      ...initialState,
+      user: testUser,
+      isAuthChecked: true,
+    });
+  });
+
+  it('should return check user failed state', () => {
+    expect(createAuthReducer(initialState, {
+      type: CHECK_USER_FAILED,
+      error: testError,
+    })).toEqual({
+      ...initialState,
+      user: null,
+      isAuthChecked: true,
+    });
+  });
+
+  it('should return logout loading state', () => {
+    expect(createAuthReducer(initialState, {
+      type: LOGOUT_LOADING,
+    })).toEqual({
+      ...initialState,
+      isLogoutLoading: true,
+    });
+  });
+
+  it('should return logout success state', () => {
+    expect(createAuthReducer(initialState, {
+      type: LOGOUT_SUCCESS,
+    })).toEqual({
+      ...initialState,
+      isLogoutLoading: false,
+      user: null,
+      isAuthChecked: false,
+    });
+  });
+
+  it('should return logout failed state', () => {
+    expect(createAuthReducer(initialState, {
+      type: LOGOUT_FAILED,
+      error: testError,
+    })).toEqual({
+      ...initialState,
+      isLogoutLoading: false,
+      user: null,
+      isAuthChecked: false,
+    });
+  });
+
 });
--- src/services/reducers/burger-ingredients/burger-ingredients.test.ts
+++ src/services/reducers/burger-ingredients/burger-ingredients.test.ts
@@ -9,46 +9,52 @@ import {IIngredient} from '../../../utils/data-types';
 
 describe('check burger-ingredients reducer', () => {
   it('should return initial state', () => {
-    expect(burgerIngredientsReducer(undefined, {} as any))
-        .toEqual(initialState);
+    expect(burgerIngredientsReducer(undefined, {} as any)).toEqual(initialState);
   });
 
   it('should return loading state', () => {
-    expect(burgerIngredientsReducer(initialState, {type: GET_INGREDIENTS_LOADING}))
-        .toEqual({
-          ...initialState,
-          isLoading: true,
-          error: null,
-        });
+    expect(burgerIngredientsReducer(initialState, {
+      type: GET_INGREDIENTS_LOADING
+    })).toEqual({
+      ...initialState,
+      isLoading: true,
+      error: null,
+    });
   });
 
   it('should return success state', () => {
     const ingredients: IIngredient[] = [];
-    expect(burgerIngredientsReducer(initialState, {type: GET_INGREDIENTS_SUCCESS, ingredients: ingredients}))
-        .toEqual({
-          ...initialState,
-          isLoading: false,
-          ingredients: [...ingredients],
-        });
+    expect(burgerIngredientsReducer(initialState, {
+      type: GET_INGREDIENTS_SUCCESS,
+      ingredients: ingredients
+    })).toEqual({
+      ...initialState,
+      isLoading: false,
+      ingredients: [...ingredients],
+    });
   });
 
   it('should return failed state', () => {
-    const error: string = '';
-    expect(burgerIngredientsReducer(initialState, {type: GET_INGREDIENTS_FAILED, error: error}))
-        .toEqual({
-          ...initialState,
-          isLoading: false,
-          error: error,
-        });
+    const error = 'error';
+    expect(burgerIngredientsReducer(initialState, {
+      type: GET_INGREDIENTS_FAILED,
+      error: error
+    })).toEqual({
+      ...initialState,
+      isLoading: false,
+      error: error,
+    });
   });
 
   it('should return select group state', () => {
-    const selectedGroup: string = '';
-    expect(burgerIngredientsReducer(initialState, {type: SELECT_INGREDIENT_GROUP, group: selectedGroup}))
-        .toEqual({
-          ...initialState,
-          selectedGroup: selectedGroup,
-        });
+    const selectedGroup = 'group';
+    expect(burgerIngredientsReducer(initialState, {
+      type: SELECT_INGREDIENT_GROUP,
+      group: selectedGroup
+    })).toEqual({
+      ...initialState,
+      selectedGroup: selectedGroup,
+    });
   });
 
 });
